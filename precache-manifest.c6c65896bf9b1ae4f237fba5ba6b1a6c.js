d3fda8dc2d5da194e7d019dbb0ecc1e8b8f2ee8a
Ronak Upadhyaya <ronakupadhyaya@Ronaks-MacBook-Pro.local>
2019-05-25 11:45:36 -0700

    Updates

---
+++ precache-manifest.c6c65896bf9b1ae4f237fba5ba6b1a6c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2db5a401a818147a659f4f6a07b4c236",
+    "url": "/index.html"
+  },
+  {
+    "revision": "726b940e8b69bf4a6a41",
+    "url": "/static/css/2.c1f9cd9a.chunk.css"
+  },
+  {
+    "revision": "6a4bccd8b23b1764ec42",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "726b940e8b69bf4a6a41",
+    "url": "/static/js/2.b3ef8d26.chunk.js"
+  },
+  {
+    "revision": "6a4bccd8b23b1764ec42",
+    "url": "/static/js/main.1e55aed4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "82163c06c0d35cebcd0bf3dc3a2c6dd2",
+    "url": "/static/media/cstep1.82163c06.png"
+  },
+  {
+    "revision": "233667a83db7e82c474ea30fffb54a29",
+    "url": "/static/media/cstep2.233667a8.png"
+  },
+  {
+    "revision": "6bd7698b45e77454024553c10e4b16c8",
+    "url": "/static/media/cstep3.6bd7698b.png"
+  },
+  {
+    "revision": "ac14fdf8ecf66aa00940909a65ae995c",
+    "url": "/static/media/gcstep1.ac14fdf8.png"
+  },
+  {
+    "revision": "d0bf2a4383691b310d8e96799cc99137",
+    "url": "/static/media/gcstep2.d0bf2a43.png"
+  },
+  {
+    "revision": "b6e99921a5d15fb2a9bf90c497cd4d13",
+    "url": "/static/media/gcstep3.b6e99921.png"
+  },
+  {
+    "revision": "b3eb7aa26dd8ea2153657aa22bae4746",
+    "url": "/static/media/gcstep4.b3eb7aa2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/citation-frontend/precache-manifest.1b94615bb20a2c968588007d1b83f3e3.js"
+  "/precache-manifest.c6c65896bf9b1ae4f237fba5ba6b1a6c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/citation-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
