dc9838a0423af1b3db472752cbf671e5063833b2
Ronak Upadhyaya <ronakupadhyaya@Ronaks-MacBook-Pro.local>
2019-05-26 16:25:36 -0700

    commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import Home from './Home';
+import Result from './Result';
+import Calendar from './Calendar';
+import GoogleCalendarInstructions from './GoogleCalendarInstructions';
+import ICalInstructions from './ICalInstructions';
+import Instructions from './Instructions';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { navigation } = this.props;
+    return (
+      <Router basename="/Citation-Frontend">
+        <Route exact path="/" component={Home} />
+        <Route path="/search" component={Result} />
+        <Route path="/calendar" component={Calendar} />
+        <Route path="/instructions" component={Instructions} />
+      </Router>
+    );
+  }
+}
