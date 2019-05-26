9b1076fa3ebc36ca09462e99f2cc72a804a70ed0
Ronak Upadhyaya <ronakupadhyaya@Ronaks-MacBook-Pro.local>
2019-05-26 16:32:05 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Citation-Frontend/precache-manifest.968426a757618e5f7ff502950b52625f.js"
+  "/Citation-Frontend/precache-manifest.fde39aceebe045cccaaa08b401e557f4.js"
 );
 
 self.addEventListener('message', (event) => {
